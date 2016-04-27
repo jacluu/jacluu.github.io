6ce3ac9f546e2a2411a60a877b056e4eb713e7df
Jacqualin Luu <luujacqualin@gmail.com>
2016-04-26 22:09:26 -0700

    website

---
+++ assets/js/script.js
@@ -0,0 +1,67 @@
+$(document).ready(function(){
+
+	$(window).scroll(function(){
+		/* This function tells you how far you have 
+		scrolled from the top off the page */
+		var offsetY = $(window).scrollTop();
+		
+	});
+
+	/* Navigation waypoint */
+	$('#content').waypoint(function(direction) {
+		if (direction == 'down') {
+			$('#nav').addClass('sticky');
+		} else {
+			$('#nav').removeClass('sticky');
+		}
+	});
+
+
+	/* Animation */
+  	$('.box').fadeOut(200);
+
+	$('.box').each(function() {
+		$(this).waypoint(function() {
+      $(this).animate({left:50, opacity:"show"}, 1000);
+	}, { offset: '70%'});
+	});
+
+
+  	$('.section-bg').fadeOut(200);
+
+	$('.section-bg').each(function() {
+		$(this).waypoint(function() {
+      $(this).animate({right:40, opacity:"show"}, 1000);
+	}, { offset: '70%'});
+	});
+
+	/* Click Scroll*/
+
+	$('#home').click(function() {
+		$('html, body').animate({
+        scrollTop: $("#header").offset().top
+    	}, 1500);
+	});
+
+
+	$('#destinations').click(function() {
+		$('html, body').animate({
+        scrollTop: $("#destination-section").offset().top
+    	}, 1500);
+	});
+
+
+	$('#community').click(function() {
+		$('html, body').animate({
+        scrollTop: $("#community-section").offset().top
+    	}, 1500);
+	});
+
+		$('#contact').click(function() {
+		$('html, body').animate({
+        scrollTop: $("#contact-section").offset().top
+    	}, 1500);
+	});
+
+
+});
